1bbdc953284f65022efbe474dce756725ac8a5fb
marco <maidh91@gmail.com>
2024-09-13 08:49:18 +1000

    acc

--- src/commands/account.ts
+++ src/commands/account.ts
@@ -26,8 +26,13 @@ export const getAccounts = async (): Promise<Account[]> => {
 // createAccount
 export const createAccount = async (account: NewAccount): Promise<Account> => {
   try {
-    const createdAccount = await invoke('create_account', { account });
-    return createdAccount as Account;
+    switch (runEnv) {
+      case RUN_ENV.DESKTOP:
+        const createdAccount = await invoke('create_account', { account });
+        return createdAccount as Account;
+      default:
+       throw new Error(`Unsupported: ${runEnv}`);
+    }
   } catch (error) {
     console.error('Error creating account:', error);
     throw error;
@@ -37,9 +42,14 @@ export const createAccount = async (account: NewAccount): Promise<Account> => {
 // updateAccount
 export const updateAccount = async (account: NewAccount): Promise<Account> => {
   try {
-    const { currency, ...updatedAccountData } = account;
-    const updatedAccount = await invoke('update_account', { account: updatedAccountData });
-    return updatedAccount as Account;
+    switch (runEnv) {
+      case RUN_ENV.DESKTOP:
+        const { currency, ...updatedAccountData } = account;
+        const updatedAccount = await invoke('update_account', { account: updatedAccountData });
+        return updatedAccount as Account;
+      default:
+       throw new Error(`Unsupported: ${runEnv}`);
+    }
   } catch (error) {
     console.error('Error updating account:', error);
     throw error;
@@ -49,7 +59,13 @@ export const updateAccount = async (account: NewAccount): Promise<Account> => {
 // deleteAccount
 export const deleteAccount = async (accountId: string): Promise<void> => {
   try {
-    await invoke('delete_account', { accountId });
+    switch (runEnv) {
+      case RUN_ENV.DESKTOP:
+        await invoke('delete_account', { accountId });
+        return;
+      default:
+       throw new Error(`Unsupported: ${runEnv}`);
+    }
   } catch (error) {
     console.error('Error deleting account:', error);
     throw error;
